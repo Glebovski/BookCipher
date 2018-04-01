886d8e6ad52a3e8fd6dc241a381ef2e2d08200f9
Gleb <zadachings@gmail.com>
2018-04-01 18:56:30 +0300

    Add project files.

---
+++ CaesarCipher/Cipher.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaesarCipher
+{
+    class Cipher 
+    {
+        public string Encrypt(string textToEncode, char[,] key)
+        {
+            string result = String.Empty;
+            
+            int m = key.GetLength(0);
+            int n = key.GetLength(1);
+            int letter = 0;
+            int problem = 0;
+            string numi = String.Empty;
+            string numj = String.Empty;
+            char[] text = textToEncode.ToArray();
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (text[letter] == key[i, j])
+                    {
+                        if (i.ToString().Length == 1 && i!=9)
+                            numi = "0" + (i+1).ToString();
+                        else numi = (i+1).ToString();
+                        if (j.ToString().Length == 1 && j!=9)
+                            numj = "0" + (j+1).ToString();
+                        else numj = (j+1).ToString();
+                        result += numi + "/" + numj + ", ";
+                        letter++;
+                    }
+                    if (letter >= textToEncode.Length && result.Length>=text.Length*7)
+                        return result;
+                    
+                    else if (letter < textToEncode.Length && j == n-1 && i == m-1)
+                    {
+                        i = 0;
+                        j = 0;
+                        problem++;
+                    }
+                    if (problem > 1)
+                    {
+                        result += numi + "/" + numj + ", ";
+                        letter++;
+                    }
+                }
+            }
+            
+            return result;
+        }
+
+        public string Decrypt(char[] textToDecode, char[,] key)
+        {
+            string result = "";
+            
+            int k = 0;
+            int p = 0;
+            for (int i = 0; i < textToDecode.Length; i+=4)
+            {
+                k = int.Parse(textToDecode[i].ToString() + textToDecode[i+1].ToString());
+                p = int.Parse(textToDecode[i+2].ToString()+textToDecode[i+3].ToString());
+                result += key[k-1, p-1];
+                
+            }
+
+            return result;
+        }
+    }
+}
---
+++ CaesarCipher/MainWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CaesarCipher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string ABCEng = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890,.()!@#$%^&*_-+=~`";
+        private string ABCUkr = " абвгдеєжзиіїйклмнопрстуфхцчшщьюяАБВГДЕЄЖЗИІЇКЛМНОПРСТУФХЦЧШЩЬЮЯ1234567890,.()!@#$%^&*_-+=~`";
+        private string Abc;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        Cipher cezar = new Cipher();
+
+        public char[,] Initialazing()
+        {
+            string text = filearea.Text;
+            string key = Move.Text.ToLower();
+            int m = Move.LineCount;
+            int n = 0;
+            if (key.Length % m != 0)
+                n = key.Length / m + 1;
+            else n = key.Length / m;
+            int letter = 0;
+            
+            char[,] poem = new char[m,n] ;
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (letter == key.Length)
+                        poem[i, j] = ' ';
+                    else poem[i, j] = key[letter];
+                    letter++;
+                }
+            }
+            return poem;
+        }
+
+        private void OpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            FileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Text Files | *.txt";
+            dialog.FilterIndex=1;
+            if (dialog.ShowDialog()==true)
+            filearea.Text= File.ReadAllText(dialog.FileName);
+        }
+
+        private void SaveFile_Click(object sender, RoutedEventArgs e)
+        {
+            FileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "Text Files | *.txt";
+            if (dialog.ShowDialog() == true)
+                File.WriteAllText(dialog.FileName,filearea.Text);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                char[,] key = Initialazing();
+                filearea.Text = cezar.Encrypt(filearea.Text, key);
+            }
+            catch {
+                
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                char[,] key = Initialazing();
+                char[] decoded = filearea.Text.Replace(",","").Replace("/","").Replace(" ","").ToArray();
+                filearea.Text = cezar.Decrypt(decoded, key);
+            }
+            catch { }
+        }
+
+        private void About_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Made by Zadachin Gleb, TM-51, #9");
+        }
+    }
+}
